d036861287ffcbac30ec96817b34100ffb065c72
Renn4nn <renanvieiracont1011@gmail.com>
2026-05-26 20:32:31 -0300

    fix: remove prisma ORM

---
+++ src/database/connection.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'carona_universitaria'
+});
+
+connection.connect((erro) => {
+
+  if (erro) {
+    console.error('Erro ao conectar no banco:', erro);
+    return;
+  }
+
+  console.log('Conectado ao MySQL');
+
+});
+
+module.exports = connection;
--- src/routes/EstudanteRoutes.js
+++ src/routes/EstudanteRoutes.js
@@ -6,5 +6,7 @@ const router = Router();
 router.post('/estudantes', EstudanteController.criar);
 router.get('/estudantes', EstudanteController.listar);
 router.get('/estudantes/:id', EstudanteController.buscarPorId);
+router.put('/estudantes/:id', EstudanteController.atualizar);
+router.delete('/estudantes/:id', EstudanteController.deletar);
 
 export default router;
